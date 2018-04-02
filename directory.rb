48b4cf863af70596056c3b52c1fda721737244d9
George Sykes <georgesykes86@gmail.com>
2018-04-02 15:08:51 +0100

    Adding code for selecting by first letter and printing only less than 12 chars

--- directory.rb
+++ directory.rb
@@ -7,7 +7,8 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    print "Now we have #{students.count} "
+    puts students.count == 1 ? "student" : "students"
     name = gets.chomp
   end
   #return the array
@@ -21,18 +22,34 @@ end
 
 #Prints all the names and cohort in order
 def print_names(names)
-  names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  names.select {|name| name[:name].size < 12}.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #Prints a summary total of students
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  print "Overall, we have #{names.count} great "
+  puts names.count == 1 ? "student" : "students"
+end
+
+def names_by_first_initial(names)
+  puts "Please enter a first name initial to search by"
+  puts "Press enter twice to print all names"
+  puts "Type exit to leave program"
+  input = gets.chomp
+  while input != "exit" do
+    print_header
+    if !input.empty?
+      print_names(names.select {|name| name[:name].chr.downcase == input.downcase})
+    else
+      print_names(names)
+    end
+    print_footer(names)
+    input = gets.chomp
+  end
 end
 
 
 students = input_students
-print_header
-print_names(students)
-print_footer(students)
+names_by_first_initial(students)
