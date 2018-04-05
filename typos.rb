cac017389fe84b12fc115fd0105dbfa718deffd1
George Sykes <georgesykes86@gmail.com>
2018-04-05 17:21:16 +0100

    Removing typos from user input while loop

--- typos.rb
+++ typos.rb
@@ -4,14 +4,14 @@ def input_students
   # create an empty array
   students = {}
   # get the first name
-  name = gets .chomp
+  name = gets.chomp
   # while the name is not empty, repeat this code
-  when name.empty? do
-  # add the student hash to the array
-  students < {:name : name :cohort : :november}
-  puts "Now we have #{student.count} students"
-  # get another name from the user
-  name = get.chomp
+  while !name.empty? do
+    # add the student hash to the array
+    students << { name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
   # return the array of students
   students
