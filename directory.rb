8ebc08328d7dbbf74b17def0eaf3c7e84509669a
George Sykes <georgesykes86@gmail.com>
2018-04-06 11:22:41 +0100

    Added load data from csv method

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list of students to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -28,11 +29,22 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
     puts "I don't know what you mean, try again"
   end
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, age, nationality, cohort = line.chomp.split(",")
+    @students << { name: name, age: age, nationality: nationality, cohort: cohort.to_sym }
+  end
+  file.close
 end
 
 #Prompts the user to input a name and stores in student array
