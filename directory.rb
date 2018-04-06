adc8de78916ff22b85b9db51fd608033e591c734
George Sykes <georgesykes86@gmail.com>
2018-04-06 10:54:40 +0100

    Add gitignore and save to csv functionality

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students to students.csv"
   puts "9. Exit"
 end
 
@@ -25,6 +26,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -76,6 +79,16 @@ def add_student(student)
                 nationality: student[:nationality] }
   print "Now we have #{@students.count} "
   puts @students.count == 1 ? "student" : "students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:age], student[:nationality], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
